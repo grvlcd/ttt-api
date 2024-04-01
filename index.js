311e3ae10196fe01861723b0ed65eaa77d6ee57c
Christian <generalpride@gmail.com>
2024-04-01 19:39:31 +0800

    revert changes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import historyRoutes from "./routes/historyRoutes.js";
-import { History } from "./models/history.model.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
@@ -11,20 +10,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/v1/histories", historyRoutes);
-
-app.get("/", async (req, res) => {
-  try {
-    const histories = await History.find();
-    res.json({
-      message: "success",
-      data: histories,
-    });
-  } catch (error) {
-    res.json({
-      message: error.message,
-    });
-  }
-});
 
 app.listen(PORT, async () => {
   await mongoose.connect(process.env.MONGO_DB_URL);
