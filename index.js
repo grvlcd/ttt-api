dfa91671d5e818ebe4e61c1c035393609fc3878e
Christian <generalpride@gmail.com>
2024-04-01 19:11:16 +0800

    updated the vercel config

--- index.js
+++ index.js
@@ -9,19 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/v1/histories", historyRoutes);
-app.get("/", async (req, res) => {
-  try {
-    await mongoose.connect(process.env.MONGO_DB_URL);
-    res.json({
-      message: "hello world",
-    });
-  } catch (error) {
-    res.json({
-      message: error.message,
-    });
-  }
-});
+app.use("/api/v1/histories", historyRoutes)
 
 app.listen(PORT, async () => {
   await mongoose.connect(process.env.MONGO_DB_URL);
